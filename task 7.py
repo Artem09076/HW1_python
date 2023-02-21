aa3704c887af77438faedb94005f6338f9dfa1d9
Artem09076 <120830078+Artem09076@users.noreply.github.com>
2023-02-21 21:10:40 +0300

    Update task 7.py

--- task 7.py
+++ task 7.py
@@ -1,10 +1,10 @@
 n=int(input())
 name= n%10
-if n >= 11 and n <= 14:
+if n >= 11 and n <= 14:#В отрезки от 11 до 14 нужно писать "korov"
         print(n, "korov")
-elif name==0 or (name>=5 and name<=9):
+elif name==0 or (name>=5 and name<=9):#Остаток от деления на 10=0 или от 5 до 9
         print(n, "korov")
-elif name==1:
+elif name==1:#Остаток от деления 1 то нужно выводить "korova"
         print(n,"korova")
-elif name>=2:
+elif name>=2:#Остаток от делиния 2 то нужно выводить "korovy"
         print(n,"korovy")
