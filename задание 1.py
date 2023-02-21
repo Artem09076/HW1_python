d1dc619289ea30b524b6ddc4f001910eb0cf1003
Artem09076 <120830078+Artem09076@users.noreply.github.com>
2023-02-21 20:18:47 +0300

    Delete задание 1.py

--- задание 1.py
+++
@@ -1,9 +0,0 @@
-k = int(input())
-m = int(input())
-n = int(input())
-if k<32000 and m<32000 and n<32000 and n>k:
-    print(((m*k)+((n-k)*m))*2)
-elif k<32000 and m<32000 and n<32000 and n<k:
-    print((n*m)*2)
-elif k<32000 and m<32000 and n<32000 and n==k:
-    print((n*m)*2)
