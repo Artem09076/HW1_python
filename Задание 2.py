ee156f1010fd1c9c12401ecf4b41139d26d09475
Artem09076 <120830078+Artem09076@users.noreply.github.com>
2023-02-21 20:25:34 +0300

    Update Задание 2.py

--- Задание 2.py
+++ Задание 2.py
@@ -2,6 +2,7 @@ x1=int(input())
 y1=int(input())
 x2=int(input())
 y2=int(input())
+#Точки находятся в одной четверти если все координаты имеет одинаковый знак
 if x1>0 and y1>0 and x2>0 and y2>0:
     print("Yes")
 elif x1<0 and y1>0 and x2<0 and y2>0:
